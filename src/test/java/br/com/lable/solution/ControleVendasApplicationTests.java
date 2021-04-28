d86b17a0b72444abffe10ac9947e7f8d6978ad4d
Alex <alex94tu@gmail.com>
2021-04-28 12:42:20 -0300

    Iniciando Projeto

---
+++ src/test/java/br/com/lable/solution/ControleVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lable.solution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
